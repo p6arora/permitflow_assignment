140be7cf744488d79ea011480bfa6a9e16a61653
Paarth Arora <paarth.arora21@gmail.com>
2024-04-17 19:34:03 -0400

    Added individual margin for x and y axis for title

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
     return (
         <div className='w-full text-lg text-center'>
-            <p className="text-center text-6xl font-bold text-blue-500 m-4">
+            <p className="text-center text-6xl font-bold text-blue-500 mx-20 my-10">
                 PermitFlow Application Process
             </p>
         </div>
