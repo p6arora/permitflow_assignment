9c0f37cce90424b15cdd7765b9eafcccda2917c2
Paarth Arora <paarth.arora21@gmail.com>
2024-04-17 18:43:14 -0400

    Removed some unneeded comments from index.ts inside my trpc Router

--- server/index.ts
+++ server/index.ts
@@ -9,24 +9,22 @@ export const appRouter = router({
             process: z.string(),
             nextSteps: z.array(z.string())
         }))
-        // Define the output schema for the mutation (optional)
-        .output(z.boolean()) // Specify that the output will be a boolean value
+        .output(z.boolean())
         .mutation(async req => {
             const { process, nextSteps } = req.input
-            // Perform some mutation logic (in this case, create a new entry in the database)
+
+            // Try to persist results into database
             try {
-                // Assuming some database operation
                 const result = await prisma.permitResult.create({
                     data: {
                         processNeeded: process,
                         steps: nextSteps
                     }
                 });
-                // Return true if the operation was successful
-                console.log(result)
                 return true;
-            } catch (error) {
+
                 // If an error occurs during the operation, return false
+            } catch (error) {
                 console.error("Error creating permit result:", error);
                 return false;
             }
