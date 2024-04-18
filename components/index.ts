2dbac32fa16c34d6c80e7d07ef05b1e3ba2f617f
Paarth Arora <paarth.arora21@gmail.com>
2024-04-18 01:54:44 -0400

    Removed unused exports

--- components/index.ts
+++ components/index.ts
@@ -1,15 +1,5 @@
-// import MainForm from './Mainform'
 import Exterior from "./Exterior"
 import Interior from "./Interior"
 import Header from "./Header"
-// import Results from './Results'
-// import Footer from './Footer'
 
-export {
-  // MainForm,
-  Exterior,
-  Interior,
-  Header,
-  // Results,
-  // Footer,
-}
+export { Exterior, Interior, Header }
